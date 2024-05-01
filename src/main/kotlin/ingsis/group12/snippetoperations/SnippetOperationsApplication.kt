653e1ab3c14cd419fba8cf28814c3b92bb880c81
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-05-01 19:40:13 -0300

    first commit

---
+++ src/main/kotlin/ingsis/group12/snippetoperations/SnippetOperationsApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.group12.snippetoperations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetOperationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetOperationsApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/group12/snippetoperations/SnippetOperationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.group12.snippetoperations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetOperationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
