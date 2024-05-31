e5956e821bf0d938ca61d3bb1a449deb014fc940
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-05-31 09:59:46 -0300

    feat: first draft of auth0

--- src/main/kotlin/ingsis/group12/snippetoperations/controller/SnippetController.kt
+++ src/main/kotlin/ingsis/group12/snippetoperations/controller/SnippetController.kt
@@ -6,11 +6,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,6 +21,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/snippet")
 @Tag(name = "Snippet")
 class SnippetController {
+    /*
+    Example endpoint to test auth0, it should require token
+     */
+    @GetMapping("/jwt")
+    fun jwt(
+        @RequestHeader("Authorization") token: String,
+        @AuthenticationPrincipal jwt: Jwt,
+    ): String {
+        return jwt.id
+    }
+
     @PostMapping()
     @ApiResponse(responseCode = "200", description = "OK")
     fun createSnippet(
--- src/test/kotlin/ingsis/group12/snippetoperations/SnippetOperationsApplicationTests.kt
+++ src/test/kotlin/ingsis/group12/snippetoperations/SnippetOperationsApplicationTests.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest
 class SnippetOperationsApplicationTests {
     @Test
     fun contextLoads() {
-        main(arrayOf("test"))
     }
 }
